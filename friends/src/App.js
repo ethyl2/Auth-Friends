c9849c97760bc225a115bfc7de50668ae0d225b6
Heather Nuffer <zzzuzz@gmail.com>
2019-12-17 13:24:00 -0700

    Added PrivateRoute and FriendsList components

--- friends/src/App.js
+++ friends/src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import PrivateRoute from './components/PrivateRoute';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
@@ -10,9 +12,12 @@ function App() {
       <header className="App-header">
         <nav>
           <Link to='/login'>Log In</Link>
+          <Link to='/friends'>Friends</Link>
         </nav>
+
       </header>
       <Switch>
+        <PrivateRoute path='/friends' component={FriendsList} />
         <Route path='/login' component={Login} />
         <Route component={Login} />
       </Switch>
---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FriendsList = props => {
+    return (
+        <div>
+            <h2>Friends</h2>
+        </div>
+    );
+};
+
+export default FriendsList;
