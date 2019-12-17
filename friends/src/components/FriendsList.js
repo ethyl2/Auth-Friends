140f39b5e477a421d46e4b0ce1b4a233890e8970
Heather Nuffer <zzzuzz@gmail.com>
2019-12-17 13:40:41 -0700

    FriendsList displays friends and login displays loading message while loading

--- friends/src/components/FriendsList.js
+++ friends/src/components/FriendsList.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+
 
 const FriendsList = props => {
+    const [friends, setFriends] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=> {
+        axiosWithAuth().get('/friends')
+        .then(res => {
+            console.log(res);
+            setFriends(res.data);
+            setLoading(false);
+        })
+        .catch(err=> console.log(err));
+    }, []);
+
     return (
         <div>
             <h2>Friends</h2>
+            {loading && <p>Loading...</p>}
+            {friends && friends.map(friend => {
+                return (<div key={friend.id}>
+                            <h3>{friend.name}</h3>
+                            <p>{friend.age} Years Old</p>
+                            <p>{friend.email}</p>
+                        </div>)})}
         </div>
     );
 };
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = ( props ) => {
     const [credentials, setCredentials] = useState({username: '', password: ''});
     const [loggingIn, setLoggingIn] = useState(false);
 
     const login = e => {
         e.preventDefault();
+        setLoggingIn(true);
         console.log(credentials);
         axiosWithAuth().post('/login', credentials)
         .then(res => {
             console.log(res);
             localStorage.setItem('token', res.data.payload);
+            setLoggingIn(false);
+            props.history.push("/friends");
         })
         .catch(err => console.log(err));
     }
@@ -23,6 +26,7 @@ const Login = () => {
     return (
         <div>
             <h2>Log In</h2>
+            {loggingIn && <p>Logging in...</p>}
             <form onSubmit={login}>
                 <label htmlFor='username'>Username: </label>
                 <input type='text'
